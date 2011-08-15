873c9f43c13359fe5426e8512a520b4e2e013532
Cody Krieger <cody@codykrieger.com>
2011-08-14 23:21:35 -0700

    Initial commit of rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SseJumble::Application.config.secret_token = '6493b9dfde5d05b3722209fe005b22464ac2229e07c170e866784b35b1d109da85b3020a5837a4701b723f1c415335041e07249bd0fffab147543b8dc2e51d8b'
