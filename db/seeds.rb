96da1d62544e112d042de08d97812fa52dc1336f
Cody Krieger <cody@codykrieger.com>
2011-08-15 01:13:28 -0700

    Sanity check before we go and add a ton of crap to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,20 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-names = File.new(File.join(Rails.root, "names.txt"))
-names.each do |line|
-  first_name = line.match(/, (.+)/).captures[0]
-  last_name = line.match(/(.+),/).captures[0]
+if Person.all.length == 0
+  names = File.new(File.join(Rails.root, "names.txt"))
+  names.each do |line|
+    first_name = line.match(/, (.+)/).captures[0]
+    last_name = line.match(/(.+),/).captures[0]
 
-  Person.create(:first_name => first_name, :last_name => last_name)
+    Person.create(:first_name => first_name, :last_name => last_name)
+  end
 end
 
-words = File.new(File.join(Rails.root, "words.txt"))
-words.each do |line|
-  Word.create(:word => line) if line.length >= 3 # min word length of 3
+if Word.all.length == 0
+  words = File.new(File.join(Rails.root, "words.txt"))
+  words.each do |line|
+    Word.create(:word => line) if line.length >= 3 # min word length of 3
+  end
 end
 
