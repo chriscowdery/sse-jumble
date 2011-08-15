09fd74c15a17de7ad5501c45f14b8b232054868a
Cody Krieger <cody@codykrieger.com>
2011-08-15 01:20:27 -0700

    Person field presence validation, added some comments re: letter distribution in seeds.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ActiveRecord::Base
   has_and_belongs_to_many :submissions
+
+  validates_presence_of :first_name, :last_name, :letter
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,20 @@ if Person.all.length == 0
     first_name = line.match(/, (.+)/).captures[0]
     last_name = line.match(/(.+),/).captures[0]
 
-    Person.create(:first_name => first_name, :last_name => last_name)
+    # TODO: assign letters to each person, keeping with the scrabble letter 
+    # distribution of:
+    #
+    # 1 point: E ×12, A ×9, I ×9, O ×8, N ×6, R ×6, T ×6, L ×4, S ×4, U ×4
+    # 2 points: D ×4, G ×3
+    # 3 points: B ×2, C ×2, M ×2, P ×2
+    # 4 points: F ×2, H ×2, V ×2, W ×2, Y ×2
+    # 5 points: K ×1
+    # 8 points: J ×1, X ×1
+    # 10 points: Q ×1, Z ×1
+
+    letter = 'a' # temporary
+
+    Person.create(:first_name => first_name, :last_name => last_name, :letter => letter)
   end
 end
 
