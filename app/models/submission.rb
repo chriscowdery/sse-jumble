dce95fde0f01835c9c3e5c67424f82caff5bfd79
Cody Krieger <cody@codykrieger.com>
2011-08-15 01:46:54 -0700

    Validation updates for models

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,6 @@
 class Submission < ActiveRecord::Base
   belongs_to :word
   has_and_belongs_to_many :people
+
+  validates_presence_of :word_id
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,5 @@
 class Word < ActiveRecord::Base
+  has_many :submissions
+
+  validates_presence_of :word
 end
