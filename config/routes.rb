33c7add2be48a63595444a4049d0031fd5fe8350
Cody Krieger <cody@codykrieger.com>
2011-08-20 04:06:10 -0700

    RootController for index, looks like auth works

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ SseJumble::Application.routes.draw do
   match '/auth/failure', :to => 'sessions#login_failure'
   match '/auth/sign_out', :to => 'sessions#sign_out'
 
+  root :to => "root#index"
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
